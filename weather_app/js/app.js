657bea3c49e8c78bb86ff2e8cd16ff5466abb952
Bugsbugme <8654808+Bugsbugme@users.noreply.github.com>
2022-04-30 23:42:39 +1200

    updated with oop

--- weather_app/js/app.js
+++ weather_app/js/app.js
@@ -3,27 +3,39 @@ const card = document.querySelector(".card");
 const details = document.querySelector(".details");
 const time = document.querySelector("img.time");
 const icon = document.querySelector(".icon img");
+const forecast = new Forecast();
 
-const updateCity = async (city) => {
-  const cityDetails = await getCity(city);
-  const cityWeather = await getWeather(cityDetails.Key);
-  return {
-    cityDetails: cityDetails,
-    cityWeather: cityWeather,
-  };
-};
-
+/* This is an event listener that is listening for a submit event on the form. When the submit event is
+triggered, the event is prevented from its default action. The city value is then trimmed and the
+form is reset. The updateCity method is then called on the forecast object and the data is passed to
+the updateUI function. The city is then stored in local storage. */
 cityForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const city = cityForm.city.value.trim();
   cityForm.reset();
 
-  updateCity(city)
+  forecast
+    .updateCity(city)
     .then((data) => updateUI(data))
     .catch((err) => console.log(err));
+
+  localStorage.setItem("city", city);
 });
 
+/* This is checking if there is a city in local storage. If there is, the forecast is updated with the
+city in local storage and the updateUI function is called. */
+if (localStorage.getItem("city")) {
+  forecast
+    .updateCity(localStorage.getItem("city"))
+    .then((data) => updateUI(data))
+    .catch((err) => console.log(err));
+}
+
+/**
+ * It takes the data object as an argument, and then it updates the UI with the data.
+ * @param data - The data object that is returned from the API.
+ */
 const updateUI = (data) => {
   // console.log(data);
   const { cityDetails, cityWeather } = data;
@@ -43,6 +55,8 @@ const updateUI = (data) => {
   let timeSrc = cityWeather.IsDayTime ? "img/day.svg" : "img/night.svg";
   time.setAttribute("src", timeSrc);
 
+  /* This is checking if the card has the class of d-none. If it does, it is removed. This is so that
+  the card is only shown when there is data to display. */
   if (card.classList.contains("d-none")) {
     card.classList.remove("d-none");
   }
--- weather_app/js/forecast.js
+++ weather_app/js/forecast.js
@@ -1,21 +1,51 @@
-const apiKey = "odEAfd5MljUw05LXzhwZOd51zpUFCru3";
+/* It takes a city name as an argument, then it returns an object containing the city details and the
+city weather. */
+class Forecast {
+  constructor() {
+    this.apiKey = "odEAfd5MljUw05LXzhwZOd51zpUFCru3";
+    this.cityURI = "http://dataservice.accuweather.com/locations/v1/cities/search";
+    this.weatherURI = "http://dataservice.accuweather.com/currentconditions/v1/";
+  }
 
-// get city information
-const getCity = async (city) => {
-  const baseUrl = "http://dataservice.accuweather.com/locations/v1/cities/search";
-  const query = `?apikey=${apiKey}&q=${city}`;
+  /**
+   * The updateCity function takes a city name as an argument, and returns an object containing the city
+   * details and the city weather.
+   * @param city - The city name
+   * @returns An object with two properties: cityDetails and cityWeather.
+   */
+  async updateCity(city) {
+    const cityDetails = await this.getCity(city);
+    const cityWeather = await this.getWeather(cityDetails.Key);
+    return {
+      cityDetails: cityDetails,
+      cityWeather: cityWeather,
+    };
+  }
 
-  const response = await fetch(baseUrl + query);
-  const data = await response.json();
-  return data[0];
-};
+  /**
+   * The getCity function takes a city name as an argument, and returns the data from the API call.
+   * @param city - The name of the city you want to get the weather for.
+   * @returns The data is being returned as an object.
+   */
+  async getCity(city) {
+    const query = `?apikey=${this.apiKey}&q=${city}`;
 
-// get weather information
-const getWeather = async (location) => {
-  const baseUrl = `http://dataservice.accuweather.com/currentconditions/v1/${location}`;
-  const query = `?apikey=${apiKey}`;
+    const response = await fetch(this.cityURI + query);
+    const data = await response.json();
+    return data[0];
+  }
 
-  const response = await fetch(baseUrl + query);
-  const data = await response.json();
-  return data[0];
-};
+  /**
+   * It takes a location as a parameter, then it creates a query string, then it fetches the data from
+   * the API, then it returns the data.
+   * @param location - The location you want to get the weather for.
+   * @returns The data is being returned as an object.
+   */
+  async getWeather(location) {
+    const query = `${location}?apikey=${this.apiKey}`;
+
+    const response = await fetch(this.weatherURI + query);
+    const data = await response.json();
+    return data[0];
+  }
+}
